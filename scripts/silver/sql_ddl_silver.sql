e8d20e8260e3eab073e0f810bc74595da7dcbc87
Vaneet singh <vaneetsingh2004@gmail.com>
2025-12-31 14:17:36 +0530

    Rename column CNTRY to cntry in erp_loc_a101

--- scripts/silver/sql_ddl_silver.sql
+++ scripts/silver/sql_ddl_silver.sql
@@ -81,7 +81,7 @@ IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
 GO
 CREATE TABLE silver.erp_loc_a101 (
 	cid NVARCHAR(50),
-	CNTRY NVARCHAR(50),
+	cntry NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
